bc3ac32217211a3c5e559e19fa118961d2847016
Justin Hanney <justin@hanney.net>
2014-02-03 21:00:17 -0800

    Show data from JSON file and add summary panels

--- web/servers.dart
+++ web/servers.dart
@@ -13,8 +13,22 @@ void main() {
 }
 
 void loadServers() {
+  TableElement table = querySelector('#servers');
+  TableSectionElement tBody = table.createTBody();
+  
   for(var server in Server.servers) {
     print('Server: $server');
+    
+    TableRowElement serverRow = tBody.insertRow(tBody.rows.length);
+    serverRow.insertCell(0).text = server['hostname'];
+    serverRow.insertCell(1).text = server['domain'];
+    serverRow.insertCell(2).text = server['dataCenter'];
+    serverRow.insertCell(3).text = server['serverType'];
+    serverRow.insertCell(4).text = server['os'];
+    serverRow.insertCell(5).text = server['cores'].toString();
+    serverRow.insertCell(6).text = server['ram'].toString();
+    serverRow.insertCell(7).text = server['hdd'].toString();
+    serverRow.insertCell(8).text = server['tco'].toString();
   }
 }
 
@@ -28,6 +42,7 @@ class Server {
   int _cores;
   int _ram;
   int _hdd;
+  int _tco;
   
   Server.fromJSON(String jsonString) {
     Map server = JSON.decode(jsonString);
@@ -38,6 +53,7 @@ class Server {
     _cores      = server['cores'];
     _ram        = server['ram'];
     _hdd        = server['hdd'];
+    _tco        = server['tco'];
   }
   
   static Future readyTheServers() {
